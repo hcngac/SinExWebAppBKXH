2458174f46113d95f833437d0fc9905fe75b31e1
Ben NG Hok Chun <hcngac@connect.ust.hk>
2017-02-14 13:28:43 +0800

    lab2 in class

---
+++ SinExWebApp20272532/Models/PackageType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SinExWebApp20272532.Models
+{
+    [Table("PackageType")]
+    public class PackageType
+    {
+        public virtual int PackageTypeID { get; set; }
+        public virtual string Type { get; set; }
+        public virtual string Description { get; set; }
+        public virtual ICollection<ServicePackageFee> ServicePackageFees { get; set; }
+    }
+}
---
+++ SinExWebApp20272532/Models/SinExDatabaseContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace SinExWebApp20272532.Models
+{
+    public class SinExDatabaseContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public SinExDatabaseContext() : base("name=SinExDatabaseContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<SinExWebApp20272532.Models.PackageType> PackageTypes { get; set; }
+
+        public System.Data.Entity.DbSet<SinExWebApp20272532.Models.ServiceType> ServiceTypes { get; set; }
+
+        public System.Data.Entity.DbSet<SinExWebApp20272532.Models.ServicePackageFee> ServicePackageFees { get; set; }
+    }
+}
