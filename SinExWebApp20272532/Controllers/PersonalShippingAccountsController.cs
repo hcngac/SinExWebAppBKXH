c2f445e31146df0070c4a543548200287acdc0ac
hcngac <hcngac@gmail.com>
2017-02-28 13:48:50 +0800

    Lab 4 Inclass Finished

---
+++ SinExWebApp20272532/Controllers/PersonalShippingAccountsController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SinExWebApp20272532.Models;
+
+namespace SinExWebApp20272532.Controllers
+{
+    public class PersonalShippingAccountsController : Controller
+    {
+        private SinExDatabaseContext db = new SinExDatabaseContext();
+
+        // GET: PersonalShippingAccounts/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PersonalShippingAccounts/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ShippingAccountId,Building,Street,City,ProvinceCode,PostalCode,CreditCardType,CreditCardNumber,CreditCardSecurityNumber,CreditCardCardholderName,CreditCardExpiryMonth,CreditCardExpiryYear,PhoneNumber,EmailAddress,FirstName,LastName")] PersonalShippingAccount personalShippingAccount)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ShippingAccounts.Add(personalShippingAccount);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(personalShippingAccount);
+        }
+
+        // GET: PersonalShippingAccounts/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PersonalShippingAccount personalShippingAccount = (PersonalShippingAccount)db.ShippingAccounts.Find(id);
+            if (personalShippingAccount == null)
+            {
+                return HttpNotFound();
+            }
+            return View(personalShippingAccount);
+        }
+
+        // POST: PersonalShippingAccounts/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ShippingAccountId,Building,Street,City,ProvinceCode,PostalCode,CreditCardType,CreditCardNumber,CreditCardSecurityNumber,CreditCardCardholderName,CreditCardExpiryMonth,CreditCardExpiryYear,PhoneNumber,EmailAddress,FirstName,LastName")] PersonalShippingAccount personalShippingAccount)
+        {
+            if (ModelState.IsValid)
+            {
+                //db.Entry(personalShippingAccount).State = EntityState.Modified;
+                //db.SaveChanges();
+                return RedirectToAction("Index", "Home");
+            }
+            return View(personalShippingAccount);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- SinExWebApp20272532/Models/ShippingAccount.cs
+++ SinExWebApp20272532/Models/ShippingAccount.cs
@@ -13,42 +13,61 @@ namespace SinExWebApp20272532.Models
         public virtual int ShippingAccountId { get; set; }
 
         // Mailing Address Information.
-        [Required]
         [StringLength(50)]
         public virtual string Building { get; set; }
+
         [Required]
         [StringLength(35)]
         public virtual string Street { get; set; }
+
         [Required]
         [StringLength(25)]
         public virtual string City { get; set; }
+
         [Required]
         [StringLength(2)]
         public virtual string ProvinceCode { get; set; }
-        [Required]
+
         [StringLength(6,MinimumLength =5)]
+        [RegularExpression(@"^[0-9]*$", ErrorMessage = "The field {0} must be a number.")]
         public virtual string PostalCode { get; set; }
 
         // Credit Card Information.
         [Required]
         public virtual string CreditCardType { get; set; }
+
         [Required]
-        [StringLength(14,MinimumLength =19)]
+        [StringLength(19,MinimumLength =14)]
+        [RegularExpression(@"^[0-9]*$", ErrorMessage = "The field {0} must be a number.")]
         public virtual string CreditCardNumber { get; set; }
+
         [Required]
         [StringLength(4,MinimumLength =3)]
+        [RegularExpression(@"^[0-9]*$", ErrorMessage = "The field {0} must be a number.")]
         public virtual string CreditCardSecurityNumber { get; set; }
+
         [Required]
         [StringLength(70)]
         public virtual string CreditCardCardholderName { get; set; }
-        [Required]
-        public virtual string CreditCardExpiryMonth { get; set; }
-        [Required]
-        public virtual string CreditCardExpiryYear { get; set; }
 
         [Required]
-        public virtual string PhoneNumber { get; set; }
+        [RegularExpression(@"^[0-9]*$", ErrorMessage = "The field {0} must be a number.")]
+        public virtual string CreditCardExpiryMonth { get; set; }
+
         [Required]
+        [RegularExpression(@"^[0-9]*$", ErrorMessage = "The field {0} must be a number.")]
+        public virtual string CreditCardExpiryYear { get; set; }
+
+        // General Information.
+
+        [Required]
+        [StringLength(14,MinimumLength =8)]
+        [RegularExpression(@"^[0-9]*$", ErrorMessage = "The field {0} must be a number.")]
+        public virtual string PhoneNumber { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        [EmailAddress()]
         public virtual string EmailAddress { get; set; }
     }
 }
