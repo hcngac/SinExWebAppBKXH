3da07d4cb39a033678a40779c9eea2fd01b792a8
Ben NG Hok Chun <Ben NG Hok Chun@DESKTOP-65I4B1F>
2017-04-24 23:59:04 +0800

    Add Pickup model

---
+++ SinExWebApp20272532/Models/Pickup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SinExWebApp20272532.Models
+{
+    [Table("Pickup")]
+    public class Pickup
+    {
+        public virtual int PickupId { get; set; }
+        public virtual DateTime PickupTime { get; set; }
+        public virtual bool IsImmediatePickup { get; set; }
+        public virtual ICollection<Shipment> Shipments { get; set; }
+    }
+}
