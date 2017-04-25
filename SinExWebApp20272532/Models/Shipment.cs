be9e95d8639ce767b0f30a98211d6a781b3ee117
Ben NG Hok Chun <Ben NG Hok Chun@DESKTOP-65I4B1F>
2017-04-25 16:47:54 +0800

    Added address model and modified shipment model

--- SinExWebApp20272532/Models/Shipment.cs
+++ SinExWebApp20272532/Models/Shipment.cs
@@ -10,26 +10,64 @@ namespace SinExWebApp20272532.Models
     [Table("Shipment")]
     public class Shipment
     {
+        // Auto-generated 16-digit numeric
         [Key]
         public virtual int                      WaybillId                   { get; set; }
+
+        // Sender information
+        [ForeignKey("Sender")]
+        public virtual int                      SenderId                    { get; set; }
+        public virtual ShippingAccount          Sender                      { get; set; }
+        [StringLength(10)]
+        [RegularExpression("[a-zA-Z0-9]*", ErrorMessage ="The {0} field must contain only alphanumeric characters.")]
         public virtual string                   ReferenceNumber             { get; set; }
-        public virtual string                   ServiceType                 { get; set; }
-        public virtual DateTime                 ShippedDate                 { get; set; }
-        public virtual DateTime                 DeliveredDate               { get; set; }
+        
+        // Recipient information
+        [Required]
+        [StringLength(70)]
+        [Display(Name = "Recipient Name")]
         public virtual string                   RecipientName               { get; set; }
-        public virtual int                      NumberOfPackages            { get; set; }
-        public virtual ICollection<Package>     Packages                    { get; set; }
-        public virtual string                   Origin                      { get; set; }
-        public virtual string                   Destination                 { get; set; }
-        public virtual string                   Status                      { get; set; }
+        [Required]
+        [StringLength(40)]
+        [Display(Name = "Company Name")]
+        public virtual string                   CompanyName                 { get; set; }
+        [StringLength(30)]
+        [Display(Name = "Department Name")]
+        public virtual string                   DepartmentName              { get; set; }
+        public virtual Address                  DeliveryAddress             { get; set; }
+        [Required]
+        [StringLength(14, MinimumLength = 8)]
+        [RegularExpression(@"^[0-9]*$", ErrorMessage = "The field {0} must be a number.")]
+        [Display(Name = "Phone Number")]
+        public virtual string PhoneNumber { get; set; }
+        [Required]
+        [StringLength(30)]
+        [EmailAddress()]
+        [Display(Name = "Email Address")]
+        public virtual string EmailAddress { get; set; }
+        [ForeignKey("Recipient")]
+        public virtual int                      RecipientId                 { get; set; }
+        public virtual ShippingAccount          Recipient                   { get; set; }
+
+        // Service information
+        public virtual string                   ServiceType                 { get; set; }
         [ForeignKey("ShipmentPayer")]
         public virtual int                      ShipmentPayerId             { get; set; }
         public virtual ShippingAccount          ShipmentPayer               { get; set; }
         [ForeignKey("TaxesDutiesPayer")]
         public virtual int                      TaxesDutiesPayerId          { get; set; }
         public virtual ShippingAccount          TaxesDutiesPayer            { get; set; }
-        public virtual int                      ShippingAccountId           { get; set; }
-        public virtual ShippingAccount          ShippingAccount             { get; set; }
+
+        // Packages information
+        public virtual int                      NumberOfPackages            { get; set; }
+        public virtual ICollection<Package>     Packages                    { get; set; }
+
+        // Meta information
+        public virtual DateTime                 ShippedDate                 { get; set; }
+        public virtual DateTime                 DeliveredDate               { get; set; }
+        public virtual string                   Origin                      { get; set; }
+        public virtual string                   Destination                 { get; set; }
+        public virtual string                   Status                      { get; set; }
         public virtual bool                     PickupEmailNotification     { get; set; }
         public virtual bool                     DeliveryEmailNotification   { get; set; }
     }
